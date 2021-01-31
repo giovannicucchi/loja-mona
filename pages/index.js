18559bb5713941f744947289e30be556bd3ae063
gcobastos@gmail.com <gcobastos@gmail.com>
2021-01-30 23:00:43 -0300

    ta ficando chato ja

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,13 @@ import Head from "next/head";
 import ProductsList from "../components/ProductsList";
 import { getProducts, getBanners } from "../utils/api";
 
-const HomePage = ({ products, categories }) => {
+const HomePage = ({ products, categories, banners }) => {
   return (
     <div>
       <Head>
         <title>Loja da Mona</title>
       </Head>
-      <ProductsList products={products} categories={categories} />
+      <ProductsList banners={banners} products={products} categories={categories} />
     </div>
   );
 };
